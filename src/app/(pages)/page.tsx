015ef752442ef208a4e7c5a20ec7f5fa8485ea96
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2023-12-01 22:05:53 +0100

    init commit

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,9 @@
+import MindMap from "@/components/MindMap"
+
+export default function Home() {
+  return (
+    <main className="flex justify-between">
+      <MindMap />
+    </main>
+  )
+}
---
+++ src/components/MindMap.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import React, { useCallback } from 'react';
+import ReactFlow, { useNodesState, useEdgesState, addEdge, Controls, MiniMap, Background } from 'reactflow';
+ 
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+function Mindmap() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <Background variant="dots" gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  )
+}
+
+export default Mindmap;
