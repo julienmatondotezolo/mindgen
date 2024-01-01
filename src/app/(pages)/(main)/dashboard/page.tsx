b24a85d3a11c3dec613a5a273facaff303c7759a
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-01-01 20:36:54 +0100

    Mindgen Templates

--- src/app/(pages)/(main)/dashboard/page.tsx
+++ src/app/(pages)/(main)/dashboard/page.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 import { HeroProfile, Navigation } from "@/components/dashboard";
+import { MindGenTemplates } from "@/components/dashboard/MindGenTemplates";
 
 export default function Dashboard() {
   return (
     <>
       <Navigation />
       <div className="flex justify-center mt-32">
-        <section className="max-w-7xl w-[90%]">
+        <section className="max-w-7xl w-[90%] space-y-12">
           <HeroProfile />
+          <MindGenTemplates />
         </section>
       </div>
     </>
