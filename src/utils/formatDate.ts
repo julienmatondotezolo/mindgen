b2d5f4263681b4c36bd49d84b3f3227f722f4a9d
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-01-12 22:25:59 +0100

    Adding today and Yesterday in date helper function

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,10 +1,9 @@
 export function formatDate(input: string) {
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   const dateObj = new Date(input);
-  const monthName = months[dateObj.getMonth()];
-  const day = dateObj.getDate();
-  const year = dateObj.getFullYear();
   const currentDate = new Date();
+  const yesterday = new Date(currentDate);
+
+  yesterday.setDate(currentDate.getDate() - 1);
 
   // Get the timezone offset in minutes
   const timezoneOffset = dateObj.getTimezoneOffset() * -1;
@@ -12,7 +11,8 @@ export function formatDate(input: string) {
   // Adjust the date and time to the correct timezone
   dateObj.setMinutes(dateObj.getMinutes() + timezoneOffset);
 
-  let formattedDate = `${monthName} ${day}, ${year}`;
+  // Initialize the formatted date
+  let formattedDate = "";
 
   // Compare the year, month, and day of both dates
   if (
@@ -27,7 +27,28 @@ export function formatDate(input: string) {
       hour12: false, // Add this line
     });
 
-    formattedDate += ` ${currentHourAndMinutes}`;
+    formattedDate = `Today, ${currentHourAndMinutes}`;
+  } else if (
+    dateObj.getFullYear() === yesterday.getFullYear() &&
+    dateObj.getMonth() === yesterday.getMonth() &&
+    dateObj.getDate() === yesterday.getDate()
+  ) {
+    // Get the hour and minutes in 24-hour format in the correct timezone
+    const hourAndMinutes = dateObj.toLocaleString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false, // Add this line
+    });
+
+    formattedDate = `Yesterday, ${hourAndMinutes}`;
+  } else {
+    // Format the date as "Month Day, Year"
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const monthName = months[dateObj.getMonth()];
+    const day = dateObj.getDate();
+    const year = dateObj.getFullYear();
+
+    formattedDate = `${monthName} ${day}, ${year}`;
   }
 
   return formattedDate;
