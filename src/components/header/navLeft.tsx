0f727934a618ec05ad36835f1c404cb25f1456b9
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-01-14 16:58:58 +0100

    Showing mindmap name in input

--- src/components/header/navLeft.tsx
+++ src/components/header/navLeft.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import hamburgerIcon from "@/assets/icons/hamburger.svg";
 import { Input } from "@/components/";
@@ -10,6 +10,10 @@ function NavLeft({ mindMapName }: { mindMapName: string | undefined }) {
   const listStyle = "p-2 bg-gray-50 rounded-xl hover:bg-gray-200";
 
   const [newMindMapName, setNewMindMapName] = useState(mindMapName || "");
+
+  useEffect(() => {
+    if (mindMapName) setNewMindMapName(mindMapName);
+  }, [mindMapName]);
 
   // Update state when input changes
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
