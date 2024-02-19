31d6273912d535a0e0c25750110e877874a7fce3
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-02-19 23:43:59 +0100

    FIX adding messages

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,15 @@
-"use client";
-
 import "../../assets/styles/globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { NextIntlClientProvider, useMessages } from "next-intl";
 import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "MindGen - Generate * with mindmap",
-//   description: "Generated everything with a mindmap",
-// };
+export const metadata: Metadata = {
+  title: "MindGen - Generate * with mindmap",
+  description: "Generated everything with a mindmap",
+};
 
 export default function RootLayout({
   children,
@@ -21,13 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  // const messages = useMessages();
-
   return (
     <html suppressHydrationWarning={true} lang={locale}>
-      <body className={`${inter.className}`}>
-        <NextIntlClientProvider locale={locale}>{children}</NextIntlClientProvider>
-      </body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
