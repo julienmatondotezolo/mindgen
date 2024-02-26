8d007c00b44bd397454e3a01c7188a920adc47d2
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-02-26 15:56:38 +0100

    Adding react hook form

--- src/app/[locale]/(pages)/(main)/profile/page.tsx
+++ src/app/[locale]/(pages)/(main)/profile/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { useTranslations } from "next-intl";
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 import { useQuery } from "react-query";
 
 import { fetchProfile } from "@/_services";
 import { ProfileProps } from "@/_types";
-import { BackDropGradient, Button, Input, Navigation } from "@/components";
+import { BackDropGradient, Button, Input, Navigation, Spinner } from "@/components";
 import { uppercaseFirstLetter } from "@/utils";
 
 const fetchUserProfile = () => fetchProfile();
@@ -16,15 +17,27 @@ export default function Profile() {
 
   const { isLoading, data: userProfile } = useQuery<ProfileProps>("userProfile", fetchUserProfile);
 
-  const [username, setUsername] = useState(userProfile?.username);
-  const [email, setEmail] = useState(userProfile?.email);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: userProfile?.username,
+      email: userProfile?.email,
+      password: "",
+      confirmNewPassword: "",
+    },
+  });
 
-  // Update state when input changes
-  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+  // Check if there are any errors
+  const hasErrors = Object.keys(errors).length > 0;
+
+  const password = watch("password");
+
+  const onSubmit = (data: any) => {
+    console.log(data);
   };
 
   return (
@@ -44,71 +57,85 @@ export default function Profile() {
                 <p>{profileText("yourCollaborations")}</p>
               </button>
             </li>
+            <li>
+              <button className="w-full flex items-center p-2 px-4 space-x-3 hover:bg-gray-100 dark:bg-slate-800 dark:hover:bg-gray-700 dark:text-white rounded-xl">
+                <p>{profileText("billing")}</p>
+              </button>
+            </li>
           </ul>
           <div className="w-[70%] p-6 rounded-2xl bg-[#f3f5f7] dark:bg-slate-500 dark:bg-opacity-20">
             <p className="text-xl mb-6 font-bold">{profileText("account")}</p>
-            <form className="space-y-8">
-              <section>
-                <p className="text-grey dark:text-grey-blue text-sm mb-2">
-                  {uppercaseFirstLetter(profileText("username"))}
-                </p>
-                <Input
-                  id="username"
-                  type="text"
-                  className="max-w-sm w-full"
-                  value={username}
-                  onChange={handleUsernameChange}
-                  placeholder={`${profileText("update")} ${profileText("username").toLowerCase()}`}
-                  required
-                  // disabled={updateMindmapMutation.isLoading}
-                />
-              </section>
-              <section>
-                <p className="text-grey dark:text-grey-blue text-sm mb-2">
-                  {uppercaseFirstLetter(profileText("email"))}
-                </p>
-                <Input
-                  id="email"
-                  type="text"
-                  className="max-w-sm w-full"
-                  value={email}
-                  onChange={handleEmailChange}
-                  placeholder={`${profileText("update")} ${profileText("email").toLowerCase()}`}
-                  required
-                  // disabled={updateMindmapMutation.isLoading}
-                />
-              </section>
-              <section>
-                <p className="mb-2 font-bold">{profileText("changePassword")}</p>
-                <div className="flex flew-wrap justify-between w-full">
-                  <section className="max-w-[48%] w-full">
-                    <p className="text-grey dark:text-grey-blue text-sm mb-2">{profileText("newPassword")}</p>
-                    <Input
-                      id="password"
-                      type="password"
-                      // value={username}
-                      // onChange={handleUsernameChange}
-                      placeholder={profileText("newPassword")}
-                      required
-                      // disabled={updateMindmapMutation.isLoading}
-                    />
-                  </section>
-                  <section className="max-w-[48%] w-full">
-                    <p className="text-grey dark:text-grey-blue text-sm mb-2">{profileText("confirmNewPassword")}</p>
-                    <Input
-                      id="confirmNewPassword"
-                      type="password"
-                      // value={username}
-                      // onChange={handleUsernameChange}
-                      placeholder={profileText("confirmNewPassword")}
-                      required
-                      // disabled={updateMindmapMutation.isLoading}
-                    />
-                  </section>
-                </div>
-              </section>
-              <Button type="submit">{uppercaseFirstLetter(text("save"))}</Button>
-            </form>
+            {isLoading ? (
+              <Spinner className="flex flex-col items-center" loadingText={`${text("loading")} `}></Spinner>
+            ) : (
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+                <section>
+                  <p className="text-grey dark:text-grey-blue text-sm mb-2">
+                    {uppercaseFirstLetter(profileText("username"))}
+                  </p>
+                  <Input
+                    id="username"
+                    type="text"
+                    className={`max-w-sm w-full ${errors.username && "border-red-500"}`}
+                    {...register("username", { required: true })}
+                    placeholder={`${profileText("update")} ${profileText("username").toLowerCase()}`}
+                    // disabled={updateMindmapMutation.isLoading}
+                  />
+                  {errors.username && <span className="text-red-500 text-sm">{text("requiredInput")}</span>}
+                </section>
+                <section>
+                  <p className="text-grey dark:text-grey-blue text-sm mb-2">
+                    {uppercaseFirstLetter(profileText("email"))}
+                  </p>
+                  <Input
+                    id="email"
+                    type="text"
+                    className={`max-w-sm w-full ${errors.email && "border-red-500"}`}
+                    {...register("email", { required: true })}
+                    placeholder={`${profileText("update")} ${profileText("email").toLowerCase()}`}
+                    // disabled={updateMindmapMutation.isLoading}
+                  />
+                  {errors.email && <span className="text-red-500 text-sm">{text("requiredInput")}</span>}
+                </section>
+                <section>
+                  <p className="mb-2 font-bold">{profileText("changePassword")}</p>
+                  <div className="flex flew-wrap justify-between w-full">
+                    <section className="max-w-[48%] w-full">
+                      <p className="text-grey dark:text-grey-blue text-sm mb-2">{profileText("newPassword")}</p>
+                      <Input
+                        id="password"
+                        type="password"
+                        {...register("password", { required: true })}
+                        placeholder={profileText("newPassword")}
+                        className={errors.password && "border-red-500"}
+                        // disabled={updateMindmapMutation.isLoading}
+                      />
+                      {errors.password && <span className="text-red-500 text-sm">{text("requiredInput")}</span>}
+                    </section>
+                    <section className="max-w-[48%] w-full">
+                      <p className="text-grey dark:text-grey-blue text-sm mb-2">{profileText("confirmNewPassword")}</p>
+                      <Input
+                        id="new-password"
+                        type="password"
+                        placeholder={profileText("confirmNewPassword")}
+                        {...register("confirmNewPassword", {
+                          required: true,
+                          validate: (value) => value === password || text("passwordNotMatching"),
+                        })}
+                        className={errors.confirmNewPassword && "border-red-500"}
+                        // disabled={updateMindmapMutation.isLoading}
+                      />
+                      {errors.confirmNewPassword && (
+                        <span className="text-red-500 text-sm">{errors.confirmNewPassword.message}</span>
+                      )}
+                    </section>
+                  </div>
+                </section>
+                <Button disabled={hasErrors} type="submit">
+                  {uppercaseFirstLetter(text("save"))}
+                </Button>
+              </form>
+            )}
           </div>
         </div>
       </div>
