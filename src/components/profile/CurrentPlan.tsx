fc9007ab1fc5e72b77d703ed699dae800ca2bd7c
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-03-02 01:55:48 +0100

    Adding currentplan

--- src/components/profile/CurrentPlan.tsx
+++ src/components/profile/CurrentPlan.tsx
@@ -1,3 +1,4 @@
+import { capitalize } from "lodash";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 import React from "react";
@@ -16,15 +17,29 @@ function CurrentPlan() {
   const navigationText = useTranslations("Navigation");
   const { isLoading, data: userProfile } = useQuery<ProfileProps>("userProfile", fetchUserProfile);
 
-  console.log("userProfile:", userProfile);
+  function calculatePercentageUsed(totalCredits: number, usedCredits: number) {
+    const percentageUsed = (usedCredits / totalCredits) * 100;
+
+    return parseFloat(percentageUsed.toFixed(2));
+  }
+
+  const usedCredits = userProfile?.usedCredits ?? 0;
+  const MAX_CREDITS = userProfile?.subscriptionDetails.MAX_CREDITS ?? 0;
+
+  const percentageUsed = calculatePercentageUsed(MAX_CREDITS, usedCredits);
 
   return (
     <div className="w-full !mt-12 p-4 bg-[#f3f5f7] dark:bg-slate-500 dark:bg-opacity-20 rounded-2xl space-y-4">
       <p className="font-bold">Manage plan</p>
-      <p className="text-sm">Free plan</p>
+      <p className="text-sm">
+        {capitalize(userProfile?.plan.toLowerCase())}: <span className="font-bold">{percentageUsed}%</span> used
+      </p>
       <section className="space-y-2">
-        <Progress value={25} />
-        <p className="text-sm text-grey dark:text-grey-blue">0.01 GB used out of 15 GB</p>
+        <Progress value={percentageUsed} />
+        <p className="text-[12px] text-grey dark:text-grey-blue">
+          <span className="font-bold">{userProfile?.usedCredits}</span> credits used out of{" "}
+          <span className="font-bold">{userProfile?.subscriptionDetails.MAX_CREDITS}</span> credits
+        </p>
       </section>
       <Link href={`/pricing`}>
         <Button className="mt-4 w-full">
