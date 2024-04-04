08d7b80c6083df5eac4d2d70c6010eee5d2cbd14
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-04-04 16:26:57 +0200

    Adding icons for node toolbar

--- src/components/mindmap/nodes/CustomNode.tsx
+++ src/components/mindmap/nodes/CustomNode.tsx
@@ -1,18 +1,30 @@
 "use client";
 
+import Image from "next/image";
 import { memo, SetStateAction, useState } from "react";
-import { Handle, Node, NodeResizer, Position, ResizeParams, useReactFlow } from "reactflow";
+import { Handle, Node, NodeResizer, Position, ResizeParams, useOnSelectionChange, useReactFlow } from "reactflow";
 
 import { CustomNodeProps } from "@/_types";
+import boldIcon from "@/assets/icons/bold.svg";
+import colorTextIcon from "@/assets/icons/colorText.svg";
+import imageIcon from "@/assets/icons/image.svg";
 import { useMindMap } from "@/hooks";
 
 const CustomNode = ({ id, data, selected, setNodes, setSourceHandle }: CustomNodeProps) => {
   const [inputText, setInputText] = useState(data.label);
+  const [isSelected, setIsSelected] = useState(false);
   const { pushToHistory } = useMindMap(undefined);
 
   const handleSize = "!w-[10px] !h-[10px]";
 
   const { getNode } = useReactFlow();
+
+  // Use the useOnSelectionChange hook to listen for selection changes
+  useOnSelectionChange({
+    onChange: ({ nodes }) => {
+      setIsSelected(nodes.some((node) => node.id === id));
+    },
+  });
 
   const resizeNode = (params: ResizeParams) => {
     const node = getNode(id);
@@ -40,6 +52,9 @@ const CustomNode = ({ id, data, selected, setNodes, setSourceHandle }: CustomNod
       }),
     );
   };
+
+  const listStyle =
+    "w-6 h-6 p-2 bg-gray-50 hover:bg-primary-opaque rounded-md dark:bg-slate-800 hover:dark:bg-slate-600";
 
   return (
     <>
@@ -89,6 +104,40 @@ const CustomNode = ({ id, data, selected, setNodes, setSourceHandle }: CustomNod
         id="left"
         className={`ml-[-15px] ${handleSize}`}
       />
+      {isSelected && (
+        <div className="mt-12 px-4 py-2 bg-white rounded-lg shadow-lg backdrop-filter backdrop-blur-lg dark:border dark:bg-slate-600 dark:bg-opacity-20 dark:border-slate-800">
+          <ul className="flex flex-row items-center justify-between">
+            <div className={`${listStyle} cursor-pointer`}>
+              <Image className="dark:invert" src={boldIcon} alt="Mouse icon" />
+            </div>
+            <div className="w-[1px] h-6 self-center mx-2 bg-slate-200 dark:bg-slate-700"></div>
+            <div className={`${listStyle} cursor-pointer`}>
+              <Image className="dark:invert cursor-pointer scale-125" src={colorTextIcon} alt="Text icon" />
+            </div>
+            <div
+              className={`${listStyle} cursor-pointer relative
+            `}
+            >
+              <figure className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-3 w-3 rounded-full bg-primary-color"></figure>
+            </div>
+            <div className="w-[1px] h-6 self-center mx-2 bg-slate-200 dark:bg-slate-700"></div>
+            <div className={`${listStyle} cursor-pointer relative`}>
+              <Image
+                className="dark:invert absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-3 w-3"
+                src={imageIcon}
+                alt="Image icon"
+              />
+            </div>
+            <div className="w-[1px] h-6 self-center mx-2 bg-slate-200 dark:bg-slate-700"></div>
+            <div className={`flex items-center ${listStyle} cursor-pointer`}>
+              <figure className=" h-[1px] w-full bg-white"></figure>
+            </div>
+            <div className={`flex items-center ${listStyle} cursor-pointer`}>
+              <figure className=" h-[2px] w-full bg-white"></figure>
+            </div>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
