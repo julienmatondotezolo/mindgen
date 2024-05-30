2748c413a8c92c940a05df4798b73127c1fc382a
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-05-30 16:51:38 +0200

    adding socket url

--- src/socket.ts
+++ src/socket.ts
@@ -1,12 +1,12 @@
 // socket.ts
 import { io } from "socket.io-client";
 
+const socketUrl: string | undefined = process.env.NEXT_PUBLIC_SOCKET_URL;
+
 // // Check if the code is running in a browser environment
 // const isBrowser = typeof window !== "undefined";
 
 // // Initialize the socket only if the code is running in the browser
 // export const socket = isBrowser ? io("http://localhost:3333") : undefined;
 
-const DEV = true;
-
-export const socket = io(`http://${DEV ? "localhost:3333" : "13.38.65.79:3333"}`);
+export const socket = io(socketUrl!);
