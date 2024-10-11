73b85d3fc57b5fbdaef8605a64ef8ffa424be756
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-10-11 10:34:56 +0200

    Styling mindmap boards

--- src/components/dashboard/RecentMindMap.tsx
+++ src/components/dashboard/RecentMindMap.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
+import { AlignJustify, LayoutGrid } from "lucide-react";
 import { useTranslations } from "next-intl";
 import React from "react";
 import { useRecoilState } from "recoil";
@@ -20,9 +21,29 @@ function RecentMindMap() {
     setIsOpen(!isOpen);
   };
 
+  const btnBackground = "p-2 rounded-md bg-gray-100 dark:bg-slate-900";
+
   return (
-    <div className="pt-8 pb-16">
-      <article className="grid sm:grid-cols-4 grid-cols-1 gap-8 w-full">
+    <div className="pb-16">
+      <section className="flex flex-wrap items-center justify-between mb-8">
+        <div className="flex space-x-2">
+          <article className={btnBackground}>
+            <p className="text-xs">Recently viewed</p>
+          </article>
+          <article className="p-2 rounded-md">
+            <p className="text-xs">Shared mindmap</p>
+          </article>
+        </div>
+        <div className="flex space-x-2">
+          <section className={btnBackground}>
+            <LayoutGrid size={15} />
+          </section>
+          <section className="p-2 rounded-md">
+            <AlignJustify size={15} />
+          </section>
+        </div>
+      </section>
+      <article className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-3 gap-8 w-full">
         {/* <div onClick={handleClick} className="cursor-pointer">
           <figure className="flex w-full h-24 border-2 border-primary-color mb-2 rounded-xl opacity-70 hover:opacity-100">
             <article className="m-auto text-primary-color text-center">
