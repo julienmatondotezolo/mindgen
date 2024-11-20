676642ec85573f447e1802625329e1008ed6051e
Julien Goldy <juliengoldy@Juliens-MacBook-Pro-4.local>
2024-11-20 12:35:59 +0100

    refactor

--- src/components/whiteboard/layers/index.tsx
+++ src/components/whiteboard/layers/index.tsx
@@ -2,5 +2,5 @@ export * from "./ellipse";
 export * from "./layerHandles";
 export * from "./rectangle";
 export * from "./selectionBox";
-export * from "./SelectionTools";
+export * from "./selectionTools";
 export * from "./shadowLayer";
