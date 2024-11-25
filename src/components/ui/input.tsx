0e18e1949e53b9c9ceb48fa6fa47d2e55c026bb3
Julien Goldy <juliengoldy@juliens-mbp-4.home>
2024-11-25 17:25:18 +0100

    Adding type toggle in input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,42 @@
 /* eslint-disable max-len */
+import { Eye, EyeOff } from "lucide-react";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => (
-  <input
-    type={type}
-    className={cn(
-      "flex h-10 w-full rounded-xl hover:border-grey border-2 bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-      className,
-    )}
-    ref={ref}
-    {...props}
-  />
-));
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="relative">
+      <input
+        type={type === "password" ? (showPassword ? "text" : "password") : type}
+        className={cn(
+          "flex h-10 w-full rounded-xl hover:border-grey border-2 bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+          type === "password" && "pr-10",
+        )}
+        ref={ref}
+        {...props}
+      />
+      {type === "password" && (
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+        >
+          {showPassword ? <EyeOff className="h-4 w-4" /> : <Eye className="h-4 w-4" />}
+        </button>
+      )}
+    </div>
+  );
+});
 
 Input.displayName = "Input";
 
